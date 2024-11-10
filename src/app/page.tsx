b94d773b3d981cb49b729d567e76ec3d0955984d
Chloé Gerry <cgerry@corp.free.fr>
2024-11-10 15:33:45 +0100

    style: update margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const Bookhloe = () => {
       <h1 className="relative z-10 text-6xl text-center pt-14 font-alex-brush lg:pt-16 lg:text-7xl">
         Bookhloé
       </h1>
-      <main className="flex flex-col items-center gap-7 relative z-10 my-20 mx-6 lg:flex-row lg:justify-center lg:gap-44 lg:my-88px">
+      <main className="flex flex-col items-center gap-7 relative z-10 my-20 mx-6 lg:flex-row lg:justify-center lg:gap-44 lg:my-88">
         {bookhloeInformations.map(({ title, subtitles }) => (
           <LargeCard key={title} title={title} subtitles={subtitles} />
         ))}
