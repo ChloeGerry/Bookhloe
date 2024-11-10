0747286aa999cd358d5f7e6599464742662445d6
Chloé Gerry <cgerry@corp.free.fr>
2024-11-10 15:38:35 +0100

    refactor: update const naming and large card style

--- src/components/Cards/LargeCard/index.tsx
+++ src/components/Cards/LargeCard/index.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
 import { bookhloeInformationsType, subtitlesType } from "@/type";
-import { LISTE_DE_LECTURE } from "@/services/constant";
+import { LISTES_DE_LECTURE } from "@/services/constant";
 
 const LargeCard = ({ title, subtitles }: bookhloeInformationsType) => {
   const backgroundColor: string =
-    title === LISTE_DE_LECTURE ? "bg-light-blue" : "bg-light-lavender";
+    title === LISTES_DE_LECTURE ? "bg-light-blue" : "bg-light-lavender";
 
   return (
     <article
-      className={`flex flex-col justify-center font-alex-brush rounded-small p-4 w-full h-44 gap-6 ${backgroundColor} text-center lg:rounded-large lg:w-96 lg:h-80 lg:p-12 lg:gap-20`}
+      className={`flex flex-col justify-center font-alex-brush rounded-medium p-4 w-full h-44 gap-6 ${backgroundColor} text-center lg:rounded-large lg:w-96 lg:h-80 lg:p-12 lg:gap-20`}
     >
-      {title === LISTE_DE_LECTURE ? (
+      {title === LISTES_DE_LECTURE ? (
         <h2 className="text-4xl lg:text-5xl">{title}</h2>
       ) : (
         <Link href="/read-books" className="text-4xl lg:text-5xl">
