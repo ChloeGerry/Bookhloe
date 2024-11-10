400527c9af7392065e6205b7e55065d0fe5629b7
Chloé Gerry <cgerry@corp.free.fr>
2024-11-10 15:41:41 +0100

    refactor: read books variable naming

--- src/app/read-books/page.tsx
+++ src/app/read-books/page.tsx
@@ -5,14 +5,14 @@ import { bookType } from "@/type";
 
 const ReadBooks = async () => {
   const books: bookType[] | null | undefined = await useBooks();
-  const wishlist = books?.filter((book) => book.read === true);
+  const readBooks = books?.filter((book) => book.read === true);
 
   return (
     <>
       <Title title="Livre lus" />
       <div className="flex flex-col mt-12 mx-4 mb-4 gap-6 justify-items-center relative z-10 md:grid md:grid-cols-2 xl:grid-cols-3 xl:gap-12 xl:m-14">
-        {wishlist &&
-          wishlist?.map(
+        {readBooks &&
+          readBooks?.map(
             ({
               author_firstname,
               author_lastname,
