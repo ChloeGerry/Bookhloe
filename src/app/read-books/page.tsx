b12ce671cac21bc43bb984c2436a8a2e3e8216c5
Chloé Gerry <cgerry@corp.free.fr>
2024-12-15 17:17:43 +0100

    refactor: add cards container in read books page

--- src/app/read-books/page.tsx
+++ src/app/read-books/page.tsx
@@ -1,43 +1,16 @@
 import Title from "@/components/Title";
-import SmallCard from "@/components/Cards/SmallCard";
+import CardsContainer from "@/components/CardsContainer";
 import { useBooks } from "@/hooks/useBooks";
 import { bookType } from "@/type";
 
 const ReadBooks = async () => {
-  const books: bookType[] | null | undefined = await useBooks();
+  const books: bookType[] | null = await useBooks();
   const readBooks = books?.filter((book) => book.read === true);
 
   return (
     <>
       <Title title="Livre lus" />
-      <div className="flex flex-col mt-12 mx-4 mb-4 gap-6 justify-items-center relative z-10 md:grid md:grid-cols-2 xl:grid-cols-3 xl:gap-12 xl:m-14">
-        {readBooks &&
-          readBooks?.map(
-            ({
-              author_firstname,
-              author_lastname,
-              title,
-              id,
-              cover_picture_url,
-              bought,
-              read,
-              grade,
-            }) => {
-              return (
-                <SmallCard
-                  key={id}
-                  authorFirstname={author_firstname}
-                  authorLastname={author_lastname}
-                  title={title}
-                  coverUrl={cover_picture_url}
-                  bought={bought}
-                  read={read}
-                  grade={grade}
-                />
-              );
-            }
-          )}
-      </div>
+      <CardsContainer booksList={readBooks} />
     </>
   );
 };
