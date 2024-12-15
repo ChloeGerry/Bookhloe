a9a18fadbffbbb936ab97c81c695e8fdde2bd62c
Chloé Gerry <cgerry@corp.free.fr>
2024-12-15 17:24:02 +0100

    refactor: remove read and buy update status from small card

--- src/components/Cards/SmallCard/index.tsx
+++ src/components/Cards/SmallCard/index.tsx
@@ -1,14 +1,20 @@
+"use client";
+
+import Link from "next/link";
+
 type SmallCardProps = {
+  id: number;
   authorFirstname: string;
   authorLastname?: string;
   title: string;
   coverUrl: string;
   bought: boolean;
   read: boolean;
-  grade?: string;
+  grade: string | null;
 };
 
 const SmallCard = ({
+  id,
   authorFirstname,
   authorLastname,
   title,
@@ -27,30 +33,23 @@ const SmallCard = ({
     cardColor = "border-light-blue";
   }
 
-  const boughtColor = bought ? "hover:bg-light-pink" : "hover:bg-light-blue";
-  const boughtBorderColor = bought ? "border-pink" : "border-blue";
-
   return (
-    <article
-      className={`flex w-full rounded-small border-2 ${cardColor} bg-[#FDF8F7] py-6 px-8 gap-8 justify-center items-center`}
-    >
-      <img
-        src={coverUrl}
-        alt="Book cover"
-        className="!w-16 !h-100 object-contain rounded-extraSmall"
-      />
-      <div className="flex flex-col gap-2">
-        <h3 className="text-gray-900 font-bold">{title}</h3>
-        <h4 className="text-gray-900">{authorFirstname + " " + authorLastname}</h4>
-        {read ? (
-          <p className="self-end text-white p-1 rounded-md bg-lavender">{grade}/20</p>
-        ) : (
-          <div
-            className={`bg-white rounded-full w-5 h-5 self-end cursor-pointer border-2 ${boughtColor} ${boughtBorderColor}`}
-          ></div>
-        )}
-      </div>
-    </article>
+    <Link href={`/read-books/${id}`} className={`flex w-full gap-8 justify-around items-center`}>
+      <article
+        className={`flex w-full rounded-small border-2 ${cardColor} bg-[#FDF8F7] py-6 px-8 gap-8 justify-around items-center`}
+      >
+        <img
+          src={coverUrl}
+          alt="Book cover"
+          className="!w-16 !h-100 object-contain rounded-extraSmall"
+        />
+        <div className="flex flex-col gap-2 items-end">
+          <h3 className="text-gray-900 font-bold">{title}</h3>
+          <h4 className="text-gray-900">{authorFirstname + " " + authorLastname}</h4>
+          {read && <p className="self-end text-white p-1 rounded-md bg-lavender">{grade}/20</p>}
+        </div>
+      </article>
+    </Link>
   );
 };
 
