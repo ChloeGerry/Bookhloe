b741a20787dc9cc7b89195b9fda0d39f87e20810
Chloé Gerry <cgerry@corp.free.fr>
2024-12-15 17:22:15 +0100

    feat: create method to update book buy status

---
+++ src/services/updateBuyBookStatus.ts
@@ -0,0 +1,29 @@
+import { bookType } from "@/type";
+
+export const updateBuyBookStatus = async (
+  id: number,
+  isBought: boolean
+): Promise<bookType | null> => {
+  try {
+    const result = await fetch(`http://localhost:3000/books/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ isBought }),
+    });
+
+    const book = await result.json();
+
+    if (!book) {
+      return null;
+    }
+
+    return book;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log("Error when updating book buy status", error);
+    }
+    throw error;
+  }
+};
