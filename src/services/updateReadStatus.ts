4dcb629463f300d874201084b0ba89b650a6212a
Chloé Gerry <cgerry@corp.free.fr>
2024-12-15 17:22:56 +0100

    feat: create method to update book read status

---
+++ src/services/updateReadStatus.ts
@@ -0,0 +1,29 @@
+import { bookType } from "@/type";
+
+export const updateReadBookStatus = async (
+  id: number,
+  isRead: boolean
+): Promise<bookType | null> => {
+  try {
+    const result = await fetch(`http://localhost:3000/books/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ isRead }),
+    });
+
+    const book = await result.json();
+
+    if (!book) {
+      return null;
+    }
+
+    return book;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log("Error when updating book read status", error);
+    }
+    throw error;
+  }
+};
