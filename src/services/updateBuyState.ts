2da5db403613e76d7a5451b11249e63ab1e07ac3
Chloé Gerry <cgerry@corp.free.fr>
2024-12-28 19:38:06 +0100

    refactor: renaming update book functions

--- src/services/updateBuyState.ts
+++ src/services/updateBuyState.ts
@@ -1,16 +1,13 @@
 import { bookType } from "@/type";
 
-export const updateReadBookStatus = async (
-  id: number,
-  isRead: boolean
-): Promise<bookType | null> => {
+export const updateBuyState = async (id: number, isBought: boolean): Promise<bookType | null> => {
   try {
     const result = await fetch(`http://localhost:3000/books/${id}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ isRead }),
+      body: JSON.stringify({ isBought }),
     });
 
     const book = await result.json();
@@ -22,7 +19,7 @@ export const updateReadBookStatus = async (
     return book;
   } catch (error) {
     if (error instanceof Error) {
-      console.log("Error when updating book read status", error);
+      console.log("Error when updating book buy status", error);
     }
     throw error;
   }
--- src/services/updateReadState.ts
+++ src/services/updateReadState.ts
@@ -1,16 +1,13 @@
 import { bookType } from "@/type";
 
-export const updateBuyBookStatus = async (
-  id: number,
-  isBought: boolean
-): Promise<bookType | null> => {
+export const updateReadState = async (id: number, isRead: boolean): Promise<bookType | null> => {
   try {
     const result = await fetch(`http://localhost:3000/books/${id}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ isBought }),
+      body: JSON.stringify({ isRead }),
     });
 
     const book = await result.json();
@@ -22,7 +19,7 @@ export const updateBuyBookStatus = async (
     return book;
   } catch (error) {
     if (error instanceof Error) {
-      console.log("Error when updating book buy status", error);
+      console.log("Error when updating book read status", error);
     }
     throw error;
   }
