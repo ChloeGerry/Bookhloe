798cc2760cfaeef8f58d65192c17f1fe0fe3a8cb
Chloé Gerry <cgerry@corp.free.fr>
2025-03-08 20:02:01 +0100

    refactor: pages naming

---
+++ src/data/bookhloePages.ts
@@ -0,0 +1,16 @@
+import { BookhloePagesType } from "@/type";
+
+export const bookhloePages: BookhloePagesType[] = [
+  {
+    title: "Whishlist",
+    href: "/whishlist",
+  },
+  {
+    title: "Pile à lire",
+    href: "/read-list",
+  },
+  {
+    title: "Livres lus",
+    href: "/read-books",
+  },
+];
