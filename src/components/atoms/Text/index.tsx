3526fb3d02d4d10d4ac094affa3b80f975042678
Chloé Gerry <cgerry@corp.free.fr>
2025-03-09 16:06:29 +0100

    feat: create text compoenent

---
+++ src/components/atoms/Text/index.tsx
@@ -0,0 +1,10 @@
+type TextProps = {
+  text: string;
+  className?: string;
+};
+
+const Text = ({ text, className }: TextProps) => {
+  return <p className={className}>{text}</p>;
+};
+
+export default Text;
